024dddf76578c1b0757945e9c09d59ee2c1bd8f6
Chuang Yao <40799379+Chuang-YAO@users.noreply.github.com>
2020-09-12 15:16:14 +0800

    Update BitArray.cpp
    substitute long with unsigned long to avoid undefined behavior on different systems

--- src/BitArray.cpp
+++ src/BitArray.cpp
@@ -5,15 +5,13 @@
 using namespace std;
 
 int main() {
-  /* Enter your code here. Read input from STDIN. Print output to STDOUT */
-
   // 1 <= N <= 10^8
   // 0 <= S, P, Q < 2^31
-  constexpr long base = 1 << 31;
-  long N, S, P, Q;
+  constexpr unsigned long base = 1UL << 31;
+  unsigned long N, S, P, Q;
   cin >> N >> S >> P >> Q;
 
-  long prev, curr, i;
+  unsigned long prev, curr, i;
   curr = prev = S % base;
   for (i = 1; i != N; ++i) {
     curr = (prev * P + Q) % base;
